479513dbf06420ebf7d3fea99f7255ff9acfc161
cristinamedrom <cristinamedranoroman@gmail.com>
2023-11-03 17:02:18 +0100

    la base el fetch

--- script.js
+++ script.js
@@ -4,5 +4,21 @@ var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
     center: [-118.243683, 34.052235],
-    zoom: 9
+    zoom: 10,
 });
+
+fetch('https://api.metro.net/LACMTA/vehicle_positions/all')
+    .then((response) =>{
+        if (!response.ok) {
+            throw new Error('No se pudo procesar la solicitud.')
+        }
+        return response.json();
+    })
+
+    .then((data) => {
+    
+    })
+
+    .catch((error) => {
+        console.error('No se pudo obtener datos.', error)
+    })
